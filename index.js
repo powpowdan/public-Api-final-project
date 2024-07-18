001c0473f15dda6a4acd76c70cd4854d2640d526
powpowdan <powpowdan@gmail.com>
2024-07-18 10:46:38 -0400

    adding two part joke

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-const API_URL = "https://secrets-api.appbrewery.com/random";
+const API_URL = " https://v2.jokeapi.dev/joke/Any?";
   
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -12,19 +12,55 @@ app.use(express.static("public"));
 // Set the view engine to ejs
 app.set('view engine', 'ejs'); 
 
-app.get("/", async (req, res) => { 
-
+app.get("/", async (req, res) => {  
     try {
-        const result = await axios.get(API_URL); 
-        res.render("index.ejs", {
-            secret: result.data.secret, 
-            user: result.data.username 
+        const result = await axios.get(API_URL);  
+
+        if (result.data.type == "twopart") { 
+          res.render("index.ejs", {
+            setup: result.data.setup,
+            delivery: result.data.delivery     
          }); 
+        } else {
+        res.render("index.ejs", {
+            joke: result.data.joke    
+         }); 
+        }
       } catch (error) {
         console.log(error.response.data);
     res.status(500);
       }
     });
+
+    app.post("/created-joke", async (req, res) => {  
+      let jokeQuery = req.body.submitted
+      
+      
+
+      if (result.data.type == "twopart") { 
+         let config = `?type=twopart&contains=${jokeQuery}`
+      } else {
+         let config = `&contains=${jokeQuery}`
+      }
+       
+      try {
+          const result = await axios.get(API_URL+config); 
+          console.log(result.data)
+        
+          if (result.data && result.data.joke) {
+      res.render("index.ejs", {
+        joke: result.data.joke
+      });
+    } else {  
+      res.redirect("/")
+    }
+        } catch (error) {
+          console.log(error.response.data);
+      res.status(500);  
+        }
+      });
+
+
 
 
     app.listen(port, () => {
