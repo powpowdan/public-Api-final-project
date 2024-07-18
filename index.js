e0e1ee042f7070037a0fdd3c45bc9babda9a2b9a
powpowdan <powpowdan@gmail.com>
2024-07-18 11:47:51 -0400

    two part joke added

--- index.js
+++ index.js
@@ -1,69 +1,71 @@
 import express from "express";
 import axios from "axios";
-import bodyParser from "body-parser"; 
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 const API_URL = " https://v2.jokeapi.dev/joke/Any?";
-  
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 // Set the view engine to ejs
-app.set('view engine', 'ejs'); 
+app.set("view engine", "ejs");
 
-app.get("/", async (req, res) => {  
-    try {
-        const result = await axios.get(API_URL);  
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL);
 
-        if (result.data.type == "twopart") { 
-          res.render("index.ejs", {
-            setup: result.data.setup,
-            delivery: result.data.delivery     
-         }); 
-        } else {
-        res.render("index.ejs", {
-            joke: result.data.joke    
-         }); 
-        }
-      } catch (error) {
-        console.log(error.response.data);
-    res.status(500);
-      }
-    });
-
-    app.post("/created-joke", async (req, res) => {  
-      let jokeQuery = req.body.submitted
-      
-      
-
-      if (result.data.type == "twopart") { 
-         let config = `?type=twopart&contains=${jokeQuery}`
-      } else {
-         let config = `&contains=${jokeQuery}`
-      }
-       
-      try {
-          const result = await axios.get(API_URL+config); 
-          console.log(result.data)
-        
-          if (result.data && result.data.joke) {
+    if (result.data.type == "twopart") {
       res.render("index.ejs", {
-        joke: result.data.joke
+        setup: result.data.setup,
+        delivery: result.data.delivery,
+        noJoke: false,
       });
-    } else {  
-      res.redirect("/")
+    } else {
+      res.render("index.ejs", {
+        joke: result.data.joke,
+        noJoke: false,
+      });
     }
-        } catch (error) {
-          console.log(error.response.data);
-      res.status(500);  
-        }
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+  }
+});
+
+app.post("/created-joke", async (req, res) => {
+  let jokeQuery = req.body.submitted;
+  let config = `&contains=${jokeQuery}`;
+
+  try {
+    const result = await axios.get(API_URL + config);
+    console.log("result type: " + result.data.type);
+    if (result.data.type == "twopart") {
+      res.render("index.ejs", {
+        setup: result.data.setup,
+        delivery: result.data.delivery,
+        noJoke: false,
       });
-
-
-
-
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
+    } else if (result.data.type == "single") {
+      res.render("index.ejs", {
+        joke: result.data.joke,
+        noJoke: false,
       });
-      
+    } else {
+      res.render("index.ejs", {
+        noJoke: true,
+      });
+    }
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+    res.render("index.ejs", {
+      noJoke: true,
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
